21fb957dd1cb966331f34a190f5dcd2ddabb1346
Pedro Bini <dev.pedrobini@gmail.com>
2020-03-31 10:11:50 -0300

    Setup ApolloProvider in Dynamic container

--- src/containers/Dynamic.js
+++ src/containers/Dynamic.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks';
 
-export default () => (
-  <div>
-    This is a dynamic page! It will not be statically exported, but is available
-    at runtime
-  </div>
-)
+import apolloClient from "../settings/apolloClient";
+
+const Dynamic = () => {
+  return (
+    <ApolloProvider client={apolloClient}>
+      <div>
+        This is a dynamic page! It will not be statically exported,
+        but is available at runtime.
+      </div>
+    </ApolloProvider>
+  );
+};
+
+export default Dynamic;
